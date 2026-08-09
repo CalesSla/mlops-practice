077aedb53a795da1cabf3115efada986f4078387
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2026-08-09 21:08:43 +0300

    pytest start

---
+++ lesson-06-pytests/preprocessing.py
@@ -0,0 +1,23 @@
+# src/preprocessing.py
+import numpy as np
+import pandas as pd
+
+
+def preprocess_data(df: pd.DataFrame) -> pd.DataFrame:
+    """Выполняет предобработку данных о поездках Uber."""
+    # 1. Фильтрация по стоимости и пассажирам
+    mask = (
+        (df["fare_amount"] > 0)
+        & (df["passenger_count"] > 0)
+        & (df["passenger_count"] <= 6)
+    )
+
+    df.drop(index=df.loc[~mask].index, inplace=True)
+
+    # 2. Создание нового признака 'distance'
+    df["distance"] = np.sqrt(
+        (df["dropoff_longitude"] - df["pickup_longitude"]) ** 2
+        + (df["dropoff_latitude"] - df["pickup_latitude"]) ** 2
+    )
+    final_features = df[["distance", "passenger_count"]]
+    return final_features
